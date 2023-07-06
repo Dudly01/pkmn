5a2354e5c15ff61dbc3239051849ee3e8f1ab484
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-07-06 13:19:19 +0200

    Add dummy OCR functions #5

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-use imageproc::contours::Contour;
+use image::imageops::crop_imm;
+use image::Luma;
+use image::{DynamicImage, GrayImage, ImageBuffer};
+use imageproc::contrast::threshold;
 use imageproc::rect::Rect;
+use imageproc::{contours::Contour, template_matching::match_template};
+use show_image::create_window;
 use std::cmp::{max, min};
 
 /// Returns the inclusive bounding box of a contour.
@@ -256,4 +261,75 @@ pub fn create_char_bitmaps() -> (Vec<char>, Vec<[u8; 49]>) {
     char_fonts.push(char);
 
     (chars, char_fonts)
+}
+
+/// Returns the best matching char for the input image.
+pub fn match_char(
+    char_image: DynamicImage,
+    known_chars: &(Vec<char>, Vec<[u8; 49]>),
+) -> Result<char, &'static str> {
+    if char_image.width() != 7 || char_image.height() != 7 {
+        return Err("Character bitmap has incorrect dimensions.");
+    }
+
+    let img_grey = &char_image.to_luma8();
+    let img_binary = threshold(&img_grey, 200);
+
+    // let window_debug = create_window("Debug Char Match", Default::default()).unwrap();
+    // window_debug
+    //     .set_image("GameBoy", img_binary.clone())
+    //     .unwrap();
+
+    let mut results: Vec<i32> = Vec::with_capacity(known_chars.0.len());
+
+    for known_bitmap in &known_chars.1 {
+        let mut total_diff = 0;
+        for (a, b) in known_bitmap.iter().zip(img_binary.pixels()) {
+            let b_value = match b {
+                Luma([v]) => v,
+            };
+
+            let diff = (*a != *b_value) as i32;
+
+            total_diff += diff
+        }
+        results.push(total_diff);
+    }
+
+    let min_index = results
+        .iter()
+        .enumerate()
+        .min_by_key(|&(_, &value)| value)
+        .map(|(index, _)| index)
+        .unwrap();
+
+    let best_match = known_chars.0[min_index];
+    Ok(best_match)
+}
+
+/// Returns the best string of best matching chars for the input image.
+pub fn match_field(
+    field_image: DynamicImage,
+    known_chars: &(Vec<char>, Vec<[u8; 49]>),
+) -> Result<String, &'static str> {
+    if field_image.height() != 7 || (field_image.width() + 1) % 8 != 0 {
+        return Err("Input dimensions are incorrect.");
+    }
+
+    let num_chars = (field_image.width() + 1) / 8;
+    let mut found_chars: Vec<_> = Vec::with_capacity(num_chars as usize);
+
+    for i in 0..num_chars {
+        let x_char = i * (7 + 1);
+        let y_char = 0;
+        let w_char = 7;
+        let h_char = 7;
+
+        let img_char = field_image.clone().crop(x_char, y_char, w_char, h_char);
+        let found_char = match_char(img_char, known_chars).unwrap();
+        found_chars.push(found_char);
+    }
+
+    let field_string: String = found_chars.into_iter().collect();
+    Ok(field_string)
 }
