8a700df2b9c9647d0fe1a49cfe3b84b79da89fc2
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-07-15 11:11:51 +0200

    Add drawing capabilities #21

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function main() {
     const start_btn = document.getElementById("start_button");
     const stop_btn = document.getElementById("stop_button");
     const snapshot_btn = document.getElementById("snapshot_button");
+    const draw_btn = document.getElementById("draw_button");
 
     var displayMediaOptions = {
         video: {
@@ -21,6 +22,9 @@ function main() {
     };
     snapshot_btn.onclick = function (e) {
         takeSnapshot();
+    };
+    draw_btn.onclick = function (e) {
+        draw();
     };
 
     async function startSharing() {
@@ -46,6 +50,26 @@ function main() {
         canvas.height = target_height;
         canvas.getContext('2d').drawImage(video, 0, 0, target_width, target_height);
         return 1;
+    };
+
+    // Draws green every second pixel of the image.
+    const draw = () => {
+        let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+        let pixelData = imageData.data;
+
+        // Iterate through the pixels of the RGBA image.
+        for (let i = 0; i < pixelData.length; i += 4) {
+            // Check if it's an even pixel (every second one)
+            if ((i / 4) % 10 === 0) {
+                pixelData[i] = 0;     // Red component
+                pixelData[i + 1] = 255; // Green component
+                pixelData[i + 2] = 0;   // Blue component
+                // The alpha component (pixelData[i + 3]) remains unchanged
+            }
+        }
+
+        // Update the modified pixel data back to the canvas
+        canvas.getContext('2d').putImageData(imageData, 0, 0);
     };
 }
 
