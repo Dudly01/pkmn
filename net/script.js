b617ddc44d2fca2b4df1016ea9089e432501b790
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-07-18 23:31:55 +0200

    Add WASM func to screen sharing page #25
    One for testing WASM by known effects.
    One for modifying the pixels to yellow.

--- net/script.js
+++ net/script.js
@@ -1,76 +1,104 @@
-function main() {
-    const video = document.getElementById("video");
-    const canvas = document.getElementById("canvas");
+import init, * as wasm from './pkg/net.js';
 
-    const start_btn = document.getElementById("start_button");
-    const stop_btn = document.getElementById("stop_button");
-    const snapshot_btn = document.getElementById("snapshot_button");
-    const draw_btn = document.getElementById("draw_button");
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
 
-    var displayMediaOptions = {
-        video: {
-            cursor: "always",
-        },
-        audio: false,
-    };
+const start_btn = document.getElementById("start_button");
+const stop_btn = document.getElementById("stop_button");
+const snapshot_btn = document.getElementById("snapshot_button");
+const draw_btn = document.getElementById("draw_button");
+const draw_wasm_btn = document.getElementById("draw_wasm_button");
+const test_wasm_btn = document.getElementById("test_wasm_button");
 
-    start_btn.onclick = function (e) {
-        startSharing();
-    };
-    stop_btn.onclick = function (e) {
-        stopSharing();
-    };
-    snapshot_btn.onclick = function (e) {
-        takeSnapshot();
-    };
-    draw_btn.onclick = function (e) {
-        draw();
-    };
+var displayMediaOptions = {
+    video: {
+        cursor: "always",
+    },
+    audio: false,
+};
 
-    async function startSharing() {
-        try {
-            video.srcObject = await navigator.mediaDevices.getDisplayMedia(
-                displayMediaOptions
-            );
-        } catch (error) {
-            console.log(error);
-        }
+start_btn.onclick = function (e) {
+    startSharing();
+};
+stop_btn.onclick = function (e) {
+    stopSharing();
+};
+snapshot_btn.onclick = function (e) {
+    takeSnapshot();
+};
+draw_btn.onclick = function (e) {
+    draw();
+};
+draw_wasm_btn.onclick = function (e) {
+    drawWasm();
+};
+test_wasm_btn.onclick = function (e) {
+    testWasm();
+};
+
+async function startSharing() {
+    try {
+        video.srcObject = await navigator.mediaDevices.getDisplayMedia(
+            displayMediaOptions
+        );
+    } catch (error) {
+        console.log(error);
     }
-
-    function stopSharing() {
-        let tracks = video.srcObject.getTracks();
-        tracks.forEach((track) => track.stop());
-        video.srcObject = null;
-    }
-
-    const takeSnapshot = () => {
-        let target_width = video.videoWidth / 2;
-        let target_height = video.videoHeight / 2;
-        canvas.width = target_width;
-        canvas.height = target_height;
-        canvas.getContext('2d').drawImage(video, 0, 0, target_width, target_height);
-        return 1;
-    };
-
-    // Draws green every second pixel of the image.
-    const draw = () => {
-        let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-        let pixelData = imageData.data;
-
-        // Iterate through the pixels of the RGBA image.
-        for (let i = 0; i < pixelData.length; i += 4) {
-            // Check if it's an even pixel (every second one)
-            if ((i / 4) % 10 === 0) {
-                pixelData[i] = 0;     // Red component
-                pixelData[i + 1] = 255; // Green component
-                pixelData[i + 2] = 0;   // Blue component
-                // The alpha component (pixelData[i + 3]) remains unchanged
-            }
-        }
-
-        // Update the modified pixel data back to the canvas
-        canvas.getContext('2d').putImageData(imageData, 0, 0);
-    };
 }
 
-main();
+function stopSharing() {
+    let tracks = video.srcObject.getTracks();
+    tracks.forEach((track) => track.stop());
+    video.srcObject = null;
+}
+
+const takeSnapshot = () => {
+    let target_width = video.videoWidth / 2;
+    let target_height = video.videoHeight / 2;
+    canvas.width = target_width;
+    canvas.height = target_height;
+    canvas.getContext('2d').drawImage(video, 0, 0, target_width, target_height);
+    return 1;
+};
+
+// Draws green every second pixel of the image.
+const draw = () => {
+    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+    let pixelData = imageData.data;
+
+    // Iterate through the pixels of the RGBA image.
+    for (let i = 0; i < pixelData.length; i += 4) {
+        // Check if it's an even pixel (every second one)
+        if ((i / 4) % 10 === 0) {
+            pixelData[i] = 0;     // Red component
+            pixelData[i + 1] = 255; // Green component
+            pixelData[i + 2] = 0;   // Blue component
+            // The alpha component (pixelData[i + 3]) remains unchanged
+        }
+    }
+
+    // Update the modified pixel data back to the canvas
+    canvas.getContext('2d').putImageData(imageData, 0, 0);
+};
+
+async function drawWasm() {
+    // Instantiate the WebAssembly module
+    await init();
+
+    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+    let pixelData = imageData.data;
+
+    wasm.draw(pixelData);
+
+    canvas.getContext('2d').putImageData(imageData, 0, 0);
+}
+
+async function testWasm() {
+    // Instantiate the WebAssembly module
+    await init();
+
+    wasm.greet("Bulbasaur");
+
+    let result = wasm.add(4, 5);
+    console.log("Result of add(4, 5):", result);
+}
--- net/src/lib.rs
+++ net/src/lib.rs
@@ -16,3 +16,16 @@ pub fn greet(name: &str) {
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
+
+#[wasm_bindgen]
+pub fn draw(pixels: &mut [u8]) {
+    let count = pixels.len();
+
+    for i in (0..count).step_by(4) {
+        if (i / 4) % 10 == 0 {
+            pixels[i] = 255; // Red component
+            pixels[i + 1] = 255; // Green component
+            pixels[i + 2] = 0; // Blue component
+        }
+    }
+}
