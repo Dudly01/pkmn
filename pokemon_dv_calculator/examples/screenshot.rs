aec25400084c126f0ddb8584ed9631daf19686a4
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-07-21 01:22:28 +0200

    Improve GameBoy localization and screenshot example user-experience (#40)
    * Improve screenshot example #38
    
    Now it exits upon an error, instead of just looping around.
    
    * Add border padding to screen locator #38
    
    Tested and found limitation.
    It fails if the input image is just the original sized Game Boy.
    But the desktop app also fails with that.
    And nobody plays that small anyway.
    
    * Increment version to 23.7.17 #38

--- pokemon_dv_calculator/examples/screenshot.rs
+++ pokemon_dv_calculator/examples/screenshot.rs
@@ -1,5 +1,4 @@
 /// Finds the GameBoy on the "screenshot.png", shows images in windows, shows stats in terminal.
-
 use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
 use show_image::{create_window, event};
@@ -11,7 +10,7 @@ use pkmn::stats::{BaseStats, DvRanges, DvTable, Experience, Stats};
 use pokemon_dv_calculator as pkmn;
 
 #[show_image::main]
-fn main() {
+fn main() -> Result<(), String> {
     let symbol_bitmaps = pkmn::ocr::create_symbol_bitmaps();
     let pkmn_base_stats = pkmn::stats::load_base_stats();
     let stats_screen_layout = StatScreen1Layout::new();
@@ -22,111 +21,110 @@ fn main() {
     // let window_erode = create_window("Erode", Default::default()).unwrap();
     let window_gameboy = create_window("GameBoy", Default::default()).unwrap();
 
-    loop {
-        let image_initial = ImageReader::open("screenshot.png")
-            .unwrap()
-            .decode()
-            .unwrap();
+    let image_initial = ImageReader::open("screenshot.png")
+        .unwrap()
+        .decode()
+        .unwrap();
 
-        let screen_pos = locate_screen(&image_initial);
-        let Some(screen_pos) = screen_pos else {
-            continue;  // Did not find gameBoy screen
-        };
+    let screen_pos = locate_screen(&image_initial);
+    let Some(screen_pos) = screen_pos else {
+        return Err("Did not find GameBoy screen".to_string());
+    };
 
-        let image_screen = image_initial.clone().crop(
-            screen_pos.x,
-            screen_pos.y,
-            screen_pos.width,
-            screen_pos.height,
-        );
-        window_gameboy
-            .set_image("GameBoy", image_screen.clone())
-            .unwrap();
+    let image_screen = image_initial.clone().crop(
+        screen_pos.x,
+        screen_pos.y,
+        screen_pos.width,
+        screen_pos.height,
+    );
+    window_gameboy
+        .set_image("GameBoy", image_screen.clone())
+        .unwrap();
 
-        let img_screen_small = image_screen.resize_exact(
-            stats_screen_layout.width as u32,
-            stats_screen_layout.height as u32,
-            FilterType::Nearest,
-        );
+    let img_screen_small = image_screen.resize_exact(
+        stats_screen_layout.width as u32,
+        stats_screen_layout.height as u32,
+        FilterType::Nearest,
+    );
 
-        let is_stats_screen = stats_screen_layout.verify_screen(&img_screen_small);
-        if !is_stats_screen {
-            continue;
-        }
-
-        let content = stats_screen_layout.read_content(&img_screen_small, &symbol_bitmaps);
-        let Ok(content) = content else {
-            continue;  // Could not read contents
-        };
-
-        let ndex: usize = content.pkmn_no.parse().unwrap();
-        let level: i32 = content.level.parse().unwrap();
-        let stats = Stats::from_screen_content(&content);
-        let record = &pkmn_base_stats[ndex - 1]; // -1 as Dex number starts with 1
-        let base_stats = BaseStats::from_record(&record);
-
-        let exp = Experience::with_no_experience();
-
-        let dv_stats_table = DvTable::new(&level, &base_stats, &exp);
-
-        dv_stats_table.print(&stats);
-
-        let dv_ranges = DvRanges::new(&stats, &dv_stats_table);
-
-        let hp = match dv_ranges.hp {
-            Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
-            None => String::from("Stat is not within expectations."),
-        };
-
-        let attack = match dv_ranges.attack {
-            Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
-            None => String::from("Stat is not within expectations."),
-        };
-
-        let defense = match dv_ranges.defense {
-            Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
-            None => String::from("Stat is not within expectations."),
-        };
-
-        let special = match dv_ranges.special {
-            Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
-            None => String::from("Stat is not within expectations."),
-        };
-
-        let speed = match dv_ranges.speed {
-            Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
-            None => String::from("Stat is not within expectations."),
-        };
-
-        println!(" HP: {:<3} DV: {}", stats.hp, hp);
-        println!("ATT: {:<3} DV: {}", stats.attack, attack);
-        println!("DEF: {:<3} DV: {}", stats.defense, defense);
-        println!("SPE: {:<3} DV: {}", stats.speed, special);
-        println!("SPC: {:<3} DV: {}", stats.special, speed);
-
-        // Print keyboard events until Escape is pressed, then exit.
-        // If the user closes the window, the channel is closed and the loop also exits.
-        let time_wait = Instant::now();
-        for event in window_gameboy.event_channel().unwrap() {
-            if let event::WindowEvent::KeyboardInput(event) = event {
-                println!("{:#?}", event);
-                if event.input.key_code == Some(event::VirtualKeyCode::Escape)
-                    && event.input.state.is_pressed()
-                {
-                    return;
-                }
-
-                if event.input.key_code == Some(event::VirtualKeyCode::S)
-                    && event.input.state.is_pressed()
-                {
-                    image_screen
-                        .save("gameboy.png")
-                        .expect("Could not save image");
-                }
-            }
-            // if time_wait.elapsed().as_millis() > 50 {
-            //     break;
-            // }
-        }
+    let is_stats_screen = stats_screen_layout.verify_screen(&img_screen_small);
+    if !is_stats_screen {
+        return Err("GameBoy not showing Summary screen 1".to_string());
     }
+
+    let content = stats_screen_layout.read_content(&img_screen_small, &symbol_bitmaps);
+    let Ok(content) = content else {
+        return Err("Could not read content from summary screen 1".to_string());
+        };
+
+    let ndex: usize = content.pkmn_no.parse().unwrap();
+    let level: i32 = content.level.parse().unwrap();
+    let stats = Stats::from_screen_content(&content);
+    let record = &pkmn_base_stats[ndex - 1]; // -1 as Dex number starts with 1
+    let base_stats = BaseStats::from_record(&record);
+
+    let exp = Experience::with_no_experience();
+
+    let dv_stats_table = DvTable::new(&level, &base_stats, &exp);
+
+    dv_stats_table.print(&stats);
+
+    let dv_ranges = DvRanges::new(&stats, &dv_stats_table);
+
+    let hp = match dv_ranges.hp {
+        Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
+        None => String::from("Stat is not within expectations."),
+    };
+
+    let attack = match dv_ranges.attack {
+        Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
+        None => String::from("Stat is not within expectations."),
+    };
+
+    let defense = match dv_ranges.defense {
+        Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
+        None => String::from("Stat is not within expectations."),
+    };
+
+    let special = match dv_ranges.special {
+        Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
+        None => String::from("Stat is not within expectations."),
+    };
+
+    let speed = match dv_ranges.speed {
+        Some(r) => format!("min {:>2} - max {:>2}", r.0, r.1),
+        None => String::from("Stat is not within expectations."),
+    };
+
+    println!(" HP: {:<3} DV: {}", stats.hp, hp);
+    println!("ATT: {:<3} DV: {}", stats.attack, attack);
+    println!("DEF: {:<3} DV: {}", stats.defense, defense);
+    println!("SPE: {:<3} DV: {}", stats.speed, special);
+    println!("SPC: {:<3} DV: {}", stats.special, speed);
+
+    // Print keyboard events until Escape is pressed, then exit.
+    // If the user closes the window, the channel is closed and the loop also exits.
+    for event in window_gameboy.event_channel().unwrap() {
+        if let event::WindowEvent::KeyboardInput(event) = event {
+            println!("{:#?}", event);
+            if event.input.key_code == Some(event::VirtualKeyCode::Escape)
+                && event.input.state.is_pressed()
+            {
+                return Ok(());
+            }
+
+            if event.input.key_code == Some(event::VirtualKeyCode::S)
+                && event.input.state.is_pressed()
+            {
+                image_screen
+                    .save("gameboy.png")
+                    .expect("Could not save image");
+            }
+        }
+        // if time_wait.elapsed().as_millis() > 50 {
+        //     break;
+        // }
+    }
+
+    Ok(())
 }
