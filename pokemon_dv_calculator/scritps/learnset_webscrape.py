f24e016b6ca48e8da3f65e7b2a083632887b8ea7
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-07-26 13:42:10 +0200

    Add func to get clean rows of the table #50

--- pokemon_dv_calculator/scritps/learnset_webscrape.py
+++ pokemon_dv_calculator/scritps/learnset_webscrape.py
@@ -1,4 +1,4 @@
-"""Collects the "By leveling up" learnset from Bulbapedia."""
+"""Collects the Gen 1 "By leveling up" learnsets from Bulbapedia."""
 
 from urllib.parse import urljoin
 
@@ -55,8 +55,8 @@ def article_url_to_source_url(url: str) -> str:
     return url_source
 
 
-def get_wiki_article_markup_source(url: str) -> str:
-    """Returns the markup source of the Bulbapedia article as a string."""
+def get_wiki_article_markdown_source(url: str) -> str:
+    """Returns the markdown source of the Bulbapedia article as a string."""
 
     source_url = article_url_to_source_url(url=url)
 
@@ -78,16 +78,107 @@ def get_wiki_article_markup_source(url: str) -> str:
     return text
 
 
+def get_pkmn(markdown_source: str) -> tuple[str, str]:
+    """Returns the Pokemon Ndex number and name from the Wiki markdown source."""
+    ndex = None
+    pkmn = None
+
+    for line in markdown_source.splitlines():
+        if line.startswith("{{pokelinkback|"):
+            line_elems = line.split("|")
+            ndex = line_elems[1]
+            pkmn = line_elems[2]
+            break
+
+    if ndex is None or pkmn is None:
+        raise ValueError("Could not find Pokemon")
+
+    return ndex, pkmn
+
+
+def get_level_up_table(markdown_source: str) -> list[list[str]]:
+    """Extracts the "By leveling up" learnset table from the WIKI markdown source.
+
+    The returned table contains the header and the rows, column by column.
+    """
+
+    markdown_rows = markdown_source.splitlines()
+
+    table_headers: list[str] = []
+    table_rows: list[str] = []
+    table_footer: list[str] = []
+    for row in markdown_rows:
+        if row.startswith("{{learnlist/levelh"):
+            table_headers.append(row)
+            continue
+
+        # Macro differs when Y version has different learnset
+        if row.startswith("{{learnlist/level1") or row.startswith("{{learnlist/levelI"):
+            table_rows.append(row)
+            continue
+
+        if row.startswith("{{learnlist/levelf"):
+            table_footer.append(row)
+            continue
+
+    if len(table_headers) != 1:
+        raise RuntimeError(
+            f"Expected one 'By leveling up' header, got {len(table_headers)}"
+        )
+
+    if len(table_rows) < 1:
+        raise RuntimeError(
+            f"Expected multiple 'By leveling up' rows, got {len(table_rows)}"
+        )
+
+    if len(table_footer) != 1:
+        raise RuntimeError(
+            f"Expected one 'By leveling up' footer, got {len(table_footer)}"
+        )
+
+    first_row = table_rows[0]
+    first_row.split("|")
+
+    header = ["Move", "Type", "Power", "Accuracy", "PP"]  # The fix columns
+
+    # Check for learnset difference between Game versions
+    if first_row[2].isalpha():  # Is the second interesting column the Moves?
+        column_count = 6
+        header.insert(0, "Level")
+    else:
+        column_count = 7
+        header_elems = table_headers[0].removesuffix("}}").split("|")
+        header.insert(0, header_elems[-1])
+        header.insert(0, header_elems[-2])
+
+    rows = []
+    for row in table_rows:
+        clean_row = row.removesuffix("}}").split("|")[1 : 1 + column_count]
+        rows.append(clean_row)
+
+    table = [header] + rows
+
+    column_count = len(table[0])
+    for row in table:
+        if len(row) != column_count:
+            raise ValueError("Mismatch of element count of rows in table.")
+
+    return table
+
+
 def main():
     article_urls = get_learnset_article_urls()
 
     if len(article_urls) != 151:
         raise RuntimeError(f"Expected URLs for 151 Pokemon. Found {len(article_urls)}.")
 
-    for url in article_urls:
-        markup_source = get_wiki_article_markup_source(url=url)
-        print(markup_source)
-        break
+    for url in article_urls[:]:
+        markdown_source = get_wiki_article_markdown_source(url=url)
+        ndex, pkmn = get_pkmn(markdown_source)
+        print(f"{ndex}, {pkmn}", end=" ")
+
+        table = get_level_up_table(markdown_source)
+        print("OK")
 
 
 if __name__ == "__main__":
