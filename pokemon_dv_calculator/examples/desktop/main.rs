d706a4bb83f25a491fc8a1329cd758d4d26311ad
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-08-11 21:53:31 +0200

    Add summary screen 2 to desktop example #60
    Now the learnset is shown when that screen is on the GameBoy #60

--- pokemon_dv_calculator/examples/desktop/main.rs
+++ pokemon_dv_calculator/examples/desktop/main.rs
@@ -26,7 +26,8 @@ fn main() -> Result<()> {
     let symbol_bitmaps = pkmn::ocr::create_symbol_bitmaps();
     let pkmn_base_stats = pkmn::stats::load_base_stats();
     let pkmn_learnsets = pkmn::learnset::load_learnsets();
-    let stats_screen_layout = pkmn::gameboy::StatScreen1Layout::new();
+    let stats_screen_1_layout = pkmn::gameboy::StatScreen1Layout::new();
+    let stats_screen_2_layout = pkmn::gameboy::StatScreen2Layout::new();
 
     loop {
         let img_screen = capturer.next_frame();
@@ -53,57 +54,79 @@ fn main() -> Result<()> {
                 gameboy_pos.height,
             )
             .resize_exact(
-                stats_screen_layout.width as u32,
-                stats_screen_layout.height as u32,
+                stats_screen_1_layout.width as u32,
+                stats_screen_1_layout.height as u32,
                 image::imageops::FilterType::Nearest,
             );
 
-        if stats_screen_layout.verify_screen(&img_gameboy) == false {
+        let is_summary_screen_1 = stats_screen_1_layout.verify_screen(&img_gameboy);
+        let is_summary_screen_2 = stats_screen_2_layout.verify_screen(&img_gameboy);
+
+        if !is_summary_screen_1 && !is_summary_screen_2 {
             stdout
                 .execute(Clear(terminal::ClearType::All))?
                 .execute(cursor::MoveTo(0, 0))?
-                .execute(Print("Not showing summary screen 1!"))?;
-            continue; // Not the screen we want
+                .execute(Print("Not showing summary screen 1 nor 2!"))?;
+            continue; // Not the screens we want
         }
 
-        let content = stats_screen_layout.read_content(&img_gameboy, &symbol_bitmaps);
-        let Ok(content) = content else {
+        if is_summary_screen_1 {
+            let content = stats_screen_1_layout.read_content(&img_gameboy, &symbol_bitmaps);
+            let Ok(content) = content else {
             stdout
                 .execute(Clear(terminal::ClearType::All))?
                 .execute(cursor::MoveTo(0, 0))?
-                .execute(Print("Could not read summary screen content!"))?;
+                .execute(Print("Could not read summary screen 1 content!"))?;
             continue;
-        };
+            };
 
-        let ndex: usize = content.pkmn_no.parse().unwrap();
-        let level: i32 = content.level.parse().unwrap();
-        let stats = pkmn::stats::Stats::from_screen_content(&content);
-        let record = &pkmn_base_stats[ndex - 1]; // -1 as Dex number starts with 1
-        let base_stats = pkmn::stats::BaseStats::from_record(&record);
+            let ndex: usize = content.pkmn_no.parse().unwrap();
+            let level: i32 = content.level.parse().unwrap();
+            let stats = pkmn::stats::Stats::from_screen_content(&content);
+            let record = &pkmn_base_stats[ndex - 1]; // -1 as Dex number starts with 1
+            let base_stats = pkmn::stats::BaseStats::from_record(&record);
 
-        let exp = pkmn::stats::Experience::with_no_experience();
+            let exp = pkmn::stats::Experience::with_no_experience();
 
-        let dv_stats_table = pkmn::stats::DvTable::new(&level, &base_stats, &exp);
+            let dv_stats_table = pkmn::stats::DvTable::new(&level, &base_stats, &exp);
 
-        let dv_ranges = pkmn::stats::DvRanges::new(&stats, &dv_stats_table);
+            let dv_ranges = pkmn::stats::DvRanges::new(&stats, &dv_stats_table);
 
-        let result = pkmn::stats::summarize_pkmn_stats(
-            record,
-            &base_stats,
-            level,
-            &stats,
-            &dv_stats_table,
-            &dv_ranges,
-        );
+            let result = pkmn::stats::summarize_pkmn_stats(
+                record,
+                &base_stats,
+                level,
+                &stats,
+                &dv_stats_table,
+                &dv_ranges,
+            );
 
-        stdout
-            .execute(Clear(terminal::ClearType::All))?
-            .execute(cursor::MoveTo(0, 0))?;
+            stdout
+                .execute(Clear(terminal::ClearType::All))?
+                .execute(cursor::MoveTo(0, 0))?;
 
-        println!("{}", result);
+            println!("{}", result);
+        }
 
-        let learnset = &pkmn_learnsets[ndex];
-        let result = pkmn::learnset::get_pretty_learnset_table(learnset).unwrap();
-        println!("{}", result);
+        if is_summary_screen_2 {
+            let content = stats_screen_2_layout.read_content(&img_gameboy, &symbol_bitmaps);
+            let Ok(content) = content else {
+            stdout
+                .execute(Clear(terminal::ClearType::All))?
+                .execute(cursor::MoveTo(0, 0))?
+                .execute(Print("Could not read summary screen 2 content!"))?;
+            continue;
+            };
+
+            let ndex: usize = content.pkmn_no.parse().unwrap();
+
+            let learnset = &pkmn_learnsets[ndex];
+            let result = pkmn::learnset::get_pretty_learnset_table(learnset).unwrap();
+
+            stdout
+                .execute(Clear(terminal::ClearType::All))?
+                .execute(cursor::MoveTo(0, 0))?;
+            println!("{}", result);
+        }
     }
 }
