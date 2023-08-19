725ff0e3b51d2926f69fcf80c696cd25ffa5efc8
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-08-19 14:35:55 +0200

    Remove snapshot manip #66
    The vutten, the JS and the Rust code all.

--- net/index.js
+++ net/index.js
@@ -5,9 +5,6 @@ const canvas = document.getElementById("canvas");
 
 const start_btn = document.getElementById("start_button");
 const stop_btn = document.getElementById("stop_button");
-const snapshot_btn = document.getElementById("snapshot_button");
-const draw_btn = document.getElementById("draw_button");
-const draw_wasm_btn = document.getElementById("draw_wasm_button");
 const gameboy_btn = document.getElementById("gameboy_button");
 
 const text_output = document.getElementById("output");
@@ -30,15 +27,6 @@ start_btn.onclick = function (e) {
 stop_btn.onclick = function (e) {
     stopSharing();
 };
-snapshot_btn.onclick = function (e) {
-    takeSnapshot();
-};
-draw_btn.onclick = function (e) {
-    draw();
-};
-draw_wasm_btn.onclick = function (e) {
-    drawWasm();
-};
 gameboy_btn.onclick = function (e) {
     gameboy();
 };
@@ -57,47 +45,6 @@ function stopSharing() {
     let tracks = video.srcObject.getTracks();
     tracks.forEach((track) => track.stop());
     video.srcObject = null;
-}
-
-const takeSnapshot = () => {
-    let target_width = video.videoWidth / 1;
-    let target_height = video.videoHeight / 1;
-    canvas.width = target_width;
-    canvas.height = target_height;
-    canvas.getContext('2d').drawImage(video, 0, 0, target_width, target_height);
-    return 1;
-};
-
-// Draws green every second pixel of the image.
-const draw = () => {
-    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelData = imageData.data;
-
-    // Iterate through the pixels of the RGBA image.
-    for (let i = 0; i < pixelData.length; i += 4) {
-        // Check if it's an even pixel (every second one)
-        if ((i / 4) % 10 === 0) {
-            pixelData[i] = 0;     // Red component
-            pixelData[i + 1] = 255; // Green component
-            pixelData[i + 2] = 0;   // Blue component
-            // The alpha component (pixelData[i + 3]) remains unchanged
-        }
-    }
-
-    // Update the modified pixel data back to the canvas
-    canvas.getContext('2d').putImageData(imageData, 0, 0);
-};
-
-async function drawWasm() {
-    // Instantiate the WebAssembly module
-    await init();
-
-    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelData = imageData.data;
-
-    wasm.draw(pixelData);
-
-    canvas.getContext('2d').putImageData(imageData, 0, 0);
 }
 
 async function gameboy() {
--- net/src/lib.rs
+++ net/src/lib.rs
@@ -21,19 +21,6 @@ pub fn add(a: u32, b: u32) -> u32 {
 }
 
 #[wasm_bindgen]
-pub fn draw(pixels: &mut [u8]) {
-    let count = pixels.len();
-
-    for i in (0..count).step_by(4) {
-        if (i / 4) % 10 == 0 {
-            pixels[i] = 255; // Red component
-            pixels[i + 1] = 255; // Green component
-            pixels[i + 2] = 0; // Blue component
-        }
-    }
-}
-
-#[wasm_bindgen]
 pub struct JsPosition {
     pub x: u32,
     pub y: u32,
