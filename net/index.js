59d60d750b9c1060f924a04ff63f10c336c0b88b
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-08-22 09:36:08 +0200

    Clear net package (#67)
    * Rename title to "PKMN Tools" #66
    
    From "Screen share".
    
    * Remove basic WASM testing functionality #66
    
    * Remove snapshot manip #66
    
    The vutten, the JS and the Rust code all.
    
    * Remove unused rust code #66
    
    It came from wasm-bindgen tutorials and from the WASM trials.
    
    * Add browser hard reset info to README #66
    
    * Change HTML buton IDs #66
    
    Previously used xxxx_button. Now its button_xxxx.
    
    * Rename button variables #66
    
    * Remove useless h2 heading #66
    
    * Add nav, main and footer element within body #66
    
    In index.html.
    Seen at other websites.
    
    * Add styles.css to modify background color #66
    
    * Increment versio to 23.8.6 #66

--- net/index.js
+++ net/index.js
@@ -3,13 +3,9 @@ import init, * as wasm from './pkg/net.js';
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 
-const start_btn = document.getElementById("start_button");
-const stop_btn = document.getElementById("stop_button");
-const snapshot_btn = document.getElementById("snapshot_button");
-const draw_btn = document.getElementById("draw_button");
-const draw_wasm_btn = document.getElementById("draw_wasm_button");
-const test_wasm_btn = document.getElementById("test_wasm_button");
-const gameboy_btn = document.getElementById("gameboy_button");
+const button_start = document.getElementById("button_start");
+const button_stop = document.getElementById("button_stop");
+const button_gameboy = document.getElementById("button_gameboy");
 
 const text_output = document.getElementById("output");
 text_output.style.cssText =
@@ -25,25 +21,13 @@ var displayMediaOptions = {
     audio: false,
 };
 
-start_btn.onclick = function (e) {
+button_start.onclick = function (e) {
     startSharing();
 };
-stop_btn.onclick = function (e) {
+button_stop.onclick = function (e) {
     stopSharing();
 };
-snapshot_btn.onclick = function (e) {
-    takeSnapshot();
-};
-draw_btn.onclick = function (e) {
-    draw();
-};
-draw_wasm_btn.onclick = function (e) {
-    drawWasm();
-};
-test_wasm_btn.onclick = function (e) {
-    testWasm();
-};
-gameboy_btn.onclick = function (e) {
+button_gameboy.onclick = function (e) {
     gameboy();
 };
 
@@ -61,57 +45,6 @@ function stopSharing() {
     let tracks = video.srcObject.getTracks();
     tracks.forEach((track) => track.stop());
     video.srcObject = null;
-}
-
-const takeSnapshot = () => {
-    let target_width = video.videoWidth / 1;
-    let target_height = video.videoHeight / 1;
-    canvas.width = target_width;
-    canvas.height = target_height;
-    canvas.getContext('2d').drawImage(video, 0, 0, target_width, target_height);
-    return 1;
-};
-
-// Draws green every second pixel of the image.
-const draw = () => {
-    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelData = imageData.data;
-
-    // Iterate through the pixels of the RGBA image.
-    for (let i = 0; i < pixelData.length; i += 4) {
-        // Check if it's an even pixel (every second one)
-        if ((i / 4) % 10 === 0) {
-            pixelData[i] = 0;     // Red component
-            pixelData[i + 1] = 255; // Green component
-            pixelData[i + 2] = 0;   // Blue component
-            // The alpha component (pixelData[i + 3]) remains unchanged
-        }
-    }
-
-    // Update the modified pixel data back to the canvas
-    canvas.getContext('2d').putImageData(imageData, 0, 0);
-};
-
-async function drawWasm() {
-    // Instantiate the WebAssembly module
-    await init();
-
-    let imageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelData = imageData.data;
-
-    wasm.draw(pixelData);
-
-    canvas.getContext('2d').putImageData(imageData, 0, 0);
-}
-
-async function testWasm() {
-    // Instantiate the WebAssembly module
-    await init();
-
-    wasm.greet("Bulbasaur");
-
-    let result = wasm.add(4, 5);
-    console.log("Result of add(4, 5):", result);
 }
 
 async function gameboy() {
