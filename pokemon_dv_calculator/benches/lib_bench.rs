da4a814c42595de165d0df7f81707255d7739214
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-09-04 12:31:57 +0200

    Add benchmark (#85)
    * Add Criterion benchmark example #83
    
    https://github.com/bheisler/criterion.rs
    https://bheisler.github.io/criterion.rs/book/criterion_rs.html
    
    * Add screen scanning benchmarks #83
    
    * Add workaround to enable command line baselines #83
    
    https://github.com/bheisler/criterion.rs/blob/master/book/src/faq.md
    https://github.com/bheisler/criterion.rs/blob/master/book/src/user_guide/command_line_options.md#baselines
    
    * Use old (pre #82) screen reading #83
    
    Now that I have the benchmarks, I want to see the difference.
    The old way was actually faster.
    
    * Try flat sampling in benchmark #83
    
    * Revert "Use old (pre #82) screen reading #83"
    
    This reverts commit abb3ac8520e857e3f33aa22c01c0c3071bc6d31f.
    
    * Use sample size isntead of flat sampling #83
    
    Trying out different options.
    
    * Remove Criterion example benchmark #83
    
    * Remove first benchmark attempt #83
    
    * Add library benchmark #83
    
    * Increment verion to 23.9.1 #83

---
+++ pokemon_dv_calculator/benches/lib_bench.rs
@@ -0,0 +1,82 @@
+/// Benchmark for the lib.
+
+use criterion::*;
+
+use image::{io::Reader as ImageReader, GrayImage};
+use pokemon_dv_calculator as pkmn;
+
+const SUMMARY_SCREEN_1_PATH: &str = "../Yellow_summary_1.png";
+const SUMMARY_SCREEN_2_PATH: &str = "../Yellow_summary_2.png";
+
+fn locate_screen(c: &mut Criterion) {
+    let mut group = c.benchmark_group("locate-screen");
+
+    for scale in [1, 2, 4, 8].iter() {
+        let id = format!("scale-{}x", scale);
+        group.bench_function(id, |b| {
+            let img = ImageReader::open(SUMMARY_SCREEN_1_PATH)
+                .unwrap()
+                .decode()
+                .unwrap();
+            let img = img.resize_exact(
+                img.width() * scale,
+                img.height() * scale,
+                image::imageops::FilterType::Nearest,
+            );
+            b.iter(|| pkmn::gameboy::locate_screen(&img));
+        });
+    }
+
+    group.finish();
+}
+
+/// Prepares the Gameboy screen image for the layout tests.
+///
+/// Loads the image from the given path, coverts it to greyscale, thresholds and lastly inverts it.
+/// This is the currently acceptable format for the functions.
+fn init_img_for_layout_tests(img_path: &str) -> GrayImage {
+    let img = ImageReader::open(img_path).unwrap().decode().unwrap();
+    let mut img = img.to_luma8();
+    imageproc::contrast::threshold_mut(&mut img, 200);
+    image::imageops::invert(&mut img);
+    img
+}
+
+fn verify_layout(c: &mut Criterion) {
+    let mut group = c.benchmark_group("verify-layout");
+
+    group.bench_function("summary-screen-1", |b| {
+        let img = init_img_for_layout_tests(SUMMARY_SCREEN_1_PATH);
+        let chars = pkmn::char::init_chars();
+        let screen_layout = pkmn::gameboy::StatScreen1Layout::new();
+        b.iter(|| screen_layout.verify_layout(&img, &chars));
+    });
+    group.bench_function("summary-screen-2", |b| {
+        let img = init_img_for_layout_tests(SUMMARY_SCREEN_2_PATH);
+        let chars = pkmn::char::init_chars();
+        let screen_layout = pkmn::gameboy::StatScreen2Layout::new();
+        b.iter(|| screen_layout.verify_layout(&img, &chars));
+    });
+    group.finish();
+}
+
+fn read_screen(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read-layout");
+
+    group.bench_function("summary-screen-1", |b| {
+        let img = init_img_for_layout_tests(SUMMARY_SCREEN_1_PATH);
+        let chars = pkmn::char::init_chars();
+        let screen_layout = pkmn::gameboy::StatScreen1Layout::new();
+        b.iter(|| screen_layout.read_fields(&img, &chars));
+    });
+    group.bench_function("summary-screen-2", |b| {
+        let img = init_img_for_layout_tests(SUMMARY_SCREEN_2_PATH);
+        let chars = pkmn::char::init_chars();
+        let screen_layout = pkmn::gameboy::StatScreen2Layout::new();
+        b.iter(|| screen_layout.read_fields(&img, &chars));
+    });
+    group.finish();
+}
+
+criterion_group!(benches, locate_screen, verify_layout, read_screen);
+criterion_main!(benches);
