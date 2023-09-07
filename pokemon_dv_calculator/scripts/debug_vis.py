e169faf191ca460cc5a5a5c21ebda48a179d5baf
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-09-07 14:09:28 +0200

    Remove rgb8 specific funcs #92
    The other funcs can handle multiple color spaces.
    Including Luma, RGB, RGBA.

--- pokemon_dv_calculator/scripts/debug_vis.py
+++ pokemon_dv_calculator/scripts/debug_vis.py
@@ -236,59 +236,6 @@ def plot_vec(image_vec, width, height, color_space):
     print("item type: {}".format(rust_type))
 
 
-def plot_rgb8_dyn_img(image_dynamic):
-    """Plots a image.DynamicImage instance."""
-    image_dynamic = debugger.unwrap(image_dynamic)
-    image_buffer = image_dynamic.GetChildAtIndex(0)
-
-    width = image_buffer.GetChildMemberWithName("width").GetValueAsUnsigned()
-    height = image_buffer.GetChildMemberWithName("height").GetValueAsUnsigned()
-    data = image_buffer.GetChildMemberWithName("data")
-    addr = data.GetChildAtIndex(0).AddressOf().GetValueAsUnsigned()
-
-    data = lldb.process.ReadMemory(addr, int(height * width) * 3, lldb.SBError())
-    data = np.frombuffer(data, dtype=np.uint8).reshape((height, width, 3))
-    plt.imshow(data, interpolation="nearest")
-    show()
-
-
-def plot_rgb8_img_buff(image_buffer):
-    """Plots an image.ImageBuffer instance."""
-    image_buffer = debugger.unwrap(image_buffer)
-
-    width = image_buffer.GetChildMemberWithName("width").GetValueAsUnsigned()
-    height = image_buffer.GetChildMemberWithName("height").GetValueAsUnsigned()
-    data = image_buffer.GetChildMemberWithName("data")
-    addr = data.GetChildAtIndex(0).AddressOf().GetValueAsUnsigned()
-
-    data = lldb.process.ReadMemory(addr, int(height * width) * 3, lldb.SBError())
-    data = np.frombuffer(data, dtype=np.uint8).reshape((height, width, 3))
-    plt.imshow(data, interpolation="nearest")
-    show()
-
-
-def plot_rgb8_vec(image_vec, xdim, ydim):
-    """Plots an RGB8 image from a vector and dimensions."""
-    image_vec = debugger.unwrap(image_vec)
-    image_addr = image_vec.GetChildAtIndex(0).AddressOf().GetValueAsUnsigned()
-
-    data = lldb.process.ReadMemory(image_addr, int(xdim * ydim) * 3, lldb.SBError())
-    data = np.frombuffer(data, dtype=np.uint8).reshape((ydim, xdim, 3))
-    plt.imshow(data, interpolation="nearest")
-    show()
-
-
-def plot_rgb8_ptr(image_ptr, xdim, ydim):
-    """Plots an RGB8 image from a pointer and dimensions."""
-    image_ptr = debugger.unwrap(image_ptr)
-    image_addr = image_ptr.GetValueAsUnsigned()
-
-    data = lldb.process.ReadMemory(image_addr, int(xdim * ydim) * 3, lldb.SBError())
-    data = np.frombuffer(data, dtype=np.uint8).reshape((ydim, xdim, 3))
-    plt.imshow(data, interpolation="nearest")
-    show()
-
-
 def info(value):
     """Writes info about the codelldb.value.Value into the terminal."""
 
