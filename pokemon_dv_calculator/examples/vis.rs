5f56c1f967ff94e1e2eedeb29694212424e9bf5f
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-09-07 10:12:37 +0200

    Add Roi to vis.rs #90
    With the region enclosing the ndex, national dex number #90

--- pokemon_dv_calculator/examples/vis.rs
+++ pokemon_dv_calculator/examples/vis.rs
@@ -1,6 +1,6 @@
 /// Helper example for developing Python debug scripts.
-
 use image::io::Reader as ImageReader;
+use pokemon_dv_calculator::{position::Position, roi::Roi};
 
 #[show_image::main]
 fn main() {
@@ -8,13 +8,24 @@ fn main() {
 
     let img_dyn = ImageReader::open(img_path).unwrap().decode().unwrap();
 
-    let img_buff = img_dyn.to_rgb32f();
+    let img_buff = img_dyn.clone().to_rgb32f();
 
     let width = img_buff.width();
     let height = img_buff.height();
     let img_buff_data = img_buff.as_raw();
 
     let img_buff_addr = img_buff_data.as_ptr();
+
+    let pos_ndex = Position {
+        x: 24,
+        y: 56,
+        width: 23,
+        height: 7,
+    };
+    let roi = Roi {
+        img: &img_dyn.to_luma8(),
+        pos: pos_ndex,
+    };
 
     println!("Done");
 }
