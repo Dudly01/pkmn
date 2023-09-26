1595f8d67d9fc737d3e4e36f25a49f630194a168
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-09-26 14:29:22 +0200

    Print result, switch func order #125

--- core/examples/locate_screen.rs
+++ core/examples/locate_screen.rs
@@ -5,39 +5,6 @@ use image::{io::Reader as ImageReader, GrayImage, Luma};
 use imageproc::{contrast::threshold_mut, rect::Rect};
 use show_image;
 
-/// Returns the position of GSC Game Boy if found.
-fn locate_screen_gsc(img: &GrayImage) -> Option<Position> {
-    let contours = imageproc::contours::find_contours::<i32>(&img);
-
-    let target_ratio = 160.0 / 62.0;
-    let tolerance = 0.01;
-
-    let mut candidates = Vec::with_capacity(4);
-
-    for contour in &contours {
-        let pos = contour_to_position(contour).unwrap();
-
-        if pos.width < 160 || pos.height < 62 {
-            continue; // Smaller than original size
-        }
-
-        let ratio = pos.width as f32 / pos.height as f32;
-        if (ratio - target_ratio).abs() > tolerance {
-            continue; // Not within tolerance
-        }
-
-        candidates.push(pos);
-    }
-
-    let largest_pos = candidates.iter().max_by_key(|p| p.width * p.height);
-
-    let Some(largest_pos) = largest_pos else {
-        return None; // There were no candidates to find the biggest of
-    };
-
-    Some(largest_pos.clone())
-}
-
 /// Returns the position of RBY Game Boy if found.
 fn locate_screen_rby(img: &GrayImage) -> Option<Position> {
     let contours = imageproc::contours::find_contours::<i32>(&img);
@@ -74,12 +41,47 @@ fn locate_screen_rby(img: &GrayImage) -> Option<Position> {
     Some(largest_pos.clone())
 }
 
+/// Returns the position of GSC Game Boy if found.
+fn locate_screen_gsc(img: &GrayImage) -> Option<Position> {
+    let contours = imageproc::contours::find_contours::<i32>(&img);
+
+    let target_ratio = 160.0 / 62.0;
+    let tolerance = 0.01;
+
+    let mut candidates = Vec::with_capacity(4);
+
+    for contour in &contours {
+        let pos = contour_to_position(contour).unwrap();
+
+        if pos.width < 160 || pos.height < 62 {
+            continue; // Smaller than original size
+        }
+
+        let ratio = pos.width as f32 / pos.height as f32;
+        if (ratio - target_ratio).abs() > tolerance {
+            continue; // Not within tolerance
+        }
+
+        candidates.push(pos);
+    }
+
+    let largest_pos = candidates.iter().max_by_key(|p| p.width * p.height);
+
+    let Some(largest_pos) = largest_pos else {
+        return None; // There were no candidates to find the biggest of
+    };
+
+    Some(largest_pos.clone())
+}
+
 #[show_image::main]
 fn main() -> Result<(), String> {
     let windows = [
         show_image::create_window("Main 1", Default::default()).unwrap(),
         show_image::create_window("2", Default::default()).unwrap(),
         show_image::create_window("3", Default::default()).unwrap(),
+        show_image::create_window("4", Default::default()).unwrap(),
+        show_image::create_window("5", Default::default()).unwrap(),
     ];
 
     use std::env;
@@ -87,6 +89,8 @@ fn main() -> Result<(), String> {
     println!("The current directory is {}", path.display());
 
     let paths = [
+        "data/Yellow_summary_1.png",
+        "data/Yellow_summary_2.png",
         "data/Crystal_summary_1.png",
         "data/Crystal_summary_2.png",
         "data/Crystal_summary_3.png",
@@ -96,14 +100,22 @@ fn main() -> Result<(), String> {
         ImageReader::open(paths[0]).unwrap().decode().unwrap(),
         ImageReader::open(paths[1]).unwrap().decode().unwrap(),
         ImageReader::open(paths[2]).unwrap().decode().unwrap(),
+        ImageReader::open(paths[3]).unwrap().decode().unwrap(),
+        ImageReader::open(paths[4]).unwrap().decode().unwrap(),
     ];
+
+    if paths.len() != imgs.len() || paths.len() != windows.len() {
+        panic!("Mismatching number of paths and images.")
+    }
 
     for threshold in (0..255).step_by(10) {
         println!("Threshold: {}", threshold);
 
-        for i in 0..3 {
+        for i in 0..5 {
             let mut img = imgs[i].to_luma8();
             threshold_mut(&mut img, threshold);
+
+            print!("{i} ");
 
             let rby_screen_pos = locate_screen_rby(&img);
             if let Some(screen_pos) = rby_screen_pos {
@@ -114,9 +126,9 @@ fn main() -> Result<(), String> {
 
                 imageproc::drawing::draw_hollow_rect_mut(&mut img, rect, color);
 
-                println!(
-                    "id:{} RBY x:{} y:{} w:{} h:{}",
-                    i, screen_pos.x, screen_pos.y, screen_pos.width, screen_pos.height
+                print!(
+                    "RBY x:{} y:{} w:{} h:{}",
+                    screen_pos.x, screen_pos.y, screen_pos.width, screen_pos.height
                 );
             }
 
@@ -132,11 +144,13 @@ fn main() -> Result<(), String> {
 
                 imageproc::drawing::draw_hollow_rect_mut(&mut img, rect, color);
 
-                println!(
-                    "id:{} GSC x:{} y:{} w:{} h:{}",
-                    i, screen_pos.x, screen_pos.y, screen_pos.width, height
+                print!(
+                    "GSC x:{} y:{} w:{} h:{}",
+                    screen_pos.x, screen_pos.y, screen_pos.width, height
                 );
             }
+
+            println!("");
 
             let window = &windows[i];
             window.set_image(i.to_string(), img).unwrap();
