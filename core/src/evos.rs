85a3ee179a50b8abeb39a378b3aa875ce05b85b9
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-10-02 12:24:19 +0200

    Add GSC evos #135

--- core/src/evos.rs
+++ core/src/evos.rs
@@ -6,3 +6,12 @@ pub fn load_evos() -> Vec<String> {
 
     evo_chains
 }
+
+/// Loads the evo chains from the file
+pub fn load_gsc_evos() -> Vec<String> {
+    const EVO_DATA: &str = include_str!("../data/genii_evo_chains.txt");
+
+    let evo_chains = EVO_DATA.lines().map(|x| x.to_owned()).collect();
+
+    evo_chains
+}
