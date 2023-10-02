3889f0e5c993a092384b2157db26c6aa83b15ed0
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-10-02 12:25:40 +0200

    Add GSC Pokemon #135

--- core/src/pokemon.rs
+++ core/src/pokemon.rs
@@ -53,3 +53,56 @@ impl Deref for Pokedex {
         &self.pokemon
     }
 }
+
+#[derive(Debug, serde::Deserialize)]
+pub struct GscPokemon {
+    #[serde(rename = "dex_number")]
+    pub ndex: i32,
+    #[serde(rename = "name")]
+    pub name: String,
+    pub type1: String,
+    pub type2: String,
+    pub hp: i32,
+    #[serde(rename = "atk")]
+    pub attack: i32,
+    #[serde(rename = "def")]
+    pub defense: i32,
+    #[serde(rename = "spa")]
+    pub special_attack: i32,
+    #[serde(rename = "spd")]
+    pub special_defense: i32,
+    #[serde(rename = "spe")]
+    pub speed: i32,
+}
+
+pub struct GscPokedex {
+    pokemon: Vec<GscPokemon>,
+}
+
+impl GscPokedex {
+    pub fn new() -> GscPokedex {
+        let mut pokedex: Vec<GscPokemon> = Vec::with_capacity(251);
+
+        const CSV_DATA: &str = include_str!("../data/smogon_gs_pokemon.csv");
+        let mut csv_reader = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .from_reader(CSV_DATA.as_bytes());
+
+        for result in csv_reader.deserialize() {
+            let record: GscPokemon = result.unwrap();
+            pokedex.push(record);
+        }
+
+        GscPokedex { pokemon: pokedex }
+    }
+}
+
+/// To have every method the inner tyoe has.
+/// https://doc.rust-lang.org/book/ch19-03-advanced-traits.html#using-the-newtype-pattern-to-implement-external-traits-on-external-types
+impl Deref for GscPokedex {
+    type Target = Vec<GscPokemon>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.pokemon
+    }
+}
