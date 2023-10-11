1dbbd49ce88fb63180c6c53c8abe935cd343ec5d
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2023-10-11 21:13:44 +0200

    Modify move formatting #165
    Widen type to accomodate Psychic.
    Remove % if no move data is given.

--- core/src/fmt.rs
+++ core/src/fmt.rs
@@ -5,7 +5,7 @@ use crate::moves::{GscMoves, Move, Moves};
 
 pub fn fmt_move_header() -> String {
     format!(
-        "{:<15}  {:<6}  {:<12}  {:>3}  {:>4}  {:>2}  {}",
+        "{:<15}  {:<7}  {:<12}  {:>3}  {:>4}  {:>2}  {}",
         "Name", "Type", "Cat", "Pow", "Acc", "PP", "Desc"
     )
 }
@@ -14,14 +14,14 @@ pub fn fmt_move(move_: Option<&Move>) -> String {
     match move_ {
         Some(m) => {
             format!(
-                "{:<15}  {:<6}  {:<12}  {:>3}  {:>3}%  {:>2}  {}",
+                "{:<15}  {:<7}  {:<12}  {:>3}  {:>3}%  {:>2}  {}",
                 m.name, m.type_, m.category, m.power, m.accuracy, m.pp, m.description
             )
         }
         None => {
             format!(
-                "{:<15}  {:<6}  {:<12}  {:>3}  {:>3}%  {:>2}  {}",
-                "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+                "{:<15}  {:<7}  {:<12}  {:>3}  {:>3}   {:>2}  {}",
+                "-", "-", "-", "-", "-", "-", "MOVE DATA NOT FOUND"
             )
         }
     }
