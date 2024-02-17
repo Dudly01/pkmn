5023f537dc95c9aede92690fd381ef0f85769ee8
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2024-02-17 12:22:45 +0100

    Add HTML creation to plotly demo #221
    Multiple approaches.
    Old nad new approach create small files. (8,9 KiB)
    New full HTML creates big file. (3,5 MiB)

--- core/scripts/plotly_demo.py
+++ core/scripts/plotly_demo.py
@@ -1,14 +1,35 @@
-"""Demo script for playing with plotly."""
+"""Demo script for playing with plotly.
+
+Sources:
+- https://plotly.com/python/imshow/
+- https://stackoverflow.com/questions/37683147
+"""
 
 
-import plotly.express as px
+from pathlib import Path
+
 import numpy as np
+import plotly
+import plotly.express as px
 
 
 def main():
     img = np.arange(15**2).reshape((15, 15))
     fig = px.imshow(img)
-    fig.show()
+
+    # Uses "old" approach
+    div = plotly.offline.plot(fig, include_plotlyjs=False, output_type="div")
+    document = f'<html><script src="https://cdn.plot.ly/plotly-latest.min.js"></script>{div}</html>'
+    Path("plotly.html").write_text(document)
+
+    # Uses "new" approach
+    div_script = plotly.io.to_html(fig, include_plotlyjs=False, full_html=False)
+    document = f'<html><script src="https://cdn.plot.ly/plotly-latest.min.js"></script>{div_script}</html>'
+    Path("plotly1.html").write_text(document)
+
+    # Returns a full standalone HTML
+    html = plotly.io.to_html(fig)
+    Path("plotly2.html").write_text(html)
 
 
 if __name__ == "__main__":
