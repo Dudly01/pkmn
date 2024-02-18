fe6f36462aafff9f882272c6aeccc684bb57b58b
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2024-02-18 20:51:04 +0100

    Configure figure #221
    In plotly demo script.

--- core/scripts/plotly_demo.py
+++ core/scripts/plotly_demo.py
@@ -17,18 +17,29 @@ def main():
     img = np.arange(15**2).reshape((15, 15))
     fig = px.imshow(img)
 
+    fig.update_layout(
+        dragmode="pan",  # Default selection no longer zoom
+        xaxis={"mirror": "allticks", "side": "top"},
+    )
+    config = {
+        "scrollZoom": True,
+        "displaylogo": False,
+    }
+
     # Uses "old" approach
-    div = plotly.offline.plot(fig, include_plotlyjs=False, output_type="div")
+    div = plotly.offline.plot(
+        fig, config=config, include_plotlyjs=False, output_type="div"
+    )
     document = f'<html><script src="https://cdn.plot.ly/plotly-latest.min.js"></script>{div}</html>'
     Path("plotly.html").write_text(document)
 
     # Uses "new" approach
-    div_script = plotly.io.to_html(fig, include_plotlyjs=False, full_html=False)
+    div_script = plotly.io.to_html(fig, config, include_plotlyjs=False, full_html=False)
     document = f'<html><script src="https://cdn.plot.ly/plotly-latest.min.js"></script>{div_script}</html>'
     Path("plotly1.html").write_text(document)
 
     # Returns a full standalone HTML
-    html = plotly.io.to_html(fig)
+    html = plotly.io.to_html(fig, config)
     Path("plotly2.html").write_text(html)
 
 
