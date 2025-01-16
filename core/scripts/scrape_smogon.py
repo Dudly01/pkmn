44b2bafb726c14290df92c305c2f29c444a51bdf
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2025-01-17 00:10:19 +0100

    Revise scrape_smogon.py #224
    Switch to regex based json data search.
    It is faster and easier to understand.
    Add more checks.
    Improve output.

--- core/scripts/scrape_smogon.py
+++ core/scripts/scrape_smogon.py
@@ -1,46 +1,58 @@
-"""Scrapes the Pokemon and Moves data from Smogon."""
+"""Scrapes the Pokemon, Items and Moves data from Smogon."""
 
 import csv
 import json
+import re
 from pathlib import Path
 
 import requests
 
 
-def get_smogon_json_string(url: str) -> str:
-    """Returns the JSON-like data from the source of the Smogon webpage."""
+def get_smogon_json(url: str) -> dict[str, list]:
+    """Returns the JSON-like data from the Smogon webpage."""
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception(
-            "Failed to fetch the webpage.", f"Status code: {response.status_code}"
+            f"Failed to fetch the webpage. Status code: {response.status_code}"
         )
     html_text = response.text
 
-    lines = html_text.splitlines()
-    json_text = None
-    substring_start = "dexSettings = "
-    substring_end = "</script>"
-    for line in lines:
-        if substring_start not in line:
-            continue
+    # The JSON data is found between
+    # <script type="text/javascript">dexSettings =
+    # and
+    # </script>
+    pattern = r'<script\s+type="text/javascript">\s*dexSettings\s*=\s*(.*?)</script>'
+    matches = list(re.finditer(pattern, html_text))
 
-        start = line.find(substring_start)
-        if start == -1:
-            raise RuntimeError("Data json start not found in HTML source")
-        start += len(substring_start)
+    if not matches:
+        raise RuntimeError("Could not find JSON data on Smogon website")
+    if len(matches) > 1:
+        raise RuntimeError(f"Expected 1 JSON data from Smogon, got {len(matches)}")
 
-        end = line.find(substring_end, start)
-        if end == -1:
-            # No </script>, then use whole line
-            end = len(line)
+    json_text = matches[0].group(1)  # Only need content of (.*?)
+    json_data = json.loads(json_text)
 
-        json_text = line[start:end]
-        break
+    try:
+        smogon_data = json_data["injectRpcs"][1][1]
+    except {KeyError, IndexError, TypeError} as e:
+        raise RuntimeError(f"Smogon JSON has unexpected format: {e}")
 
-    if json_text is None:
-        raise RuntimeError("Did not find JSON-like data in SMOGON html.")
+    # Similar elements to sidebar of webpage
+    expected_elems = (
+        "pokemon",
+        "moves",
+        "abilities",
+        "items",
+        "types",
+        "formats",
+        "natures",
+        "moveflags",
+    )
+    for elem in expected_elems:
+        if elem not in smogon_data:
+            raise ValueError(f"Smogon JSON has unexpected format: {elem} not found")
 
-    return json_text
+    return smogon_data
 
 
 def export_moves(smogon_json: dict, dst_path: Path) -> None:
@@ -57,13 +69,14 @@ def export_moves(smogon_json: dict, dst_path: Path) -> None:
     ]
 
     rows = []
-    for move_dict in smogon_json["injectRpcs"][1][1]["moves"]:
+    for move_dict in smogon_json["moves"]:
         row = [move_dict[col] for col in header]
         rows.append(row)
 
     rows.sort(key=lambda x: x[0])  # Sort moves by name
 
-    with dst_path.open("w", encoding="utf-8") as f:
+    # Specifying newline avoids extra lines on Windows
+    with dst_path.open("w", encoding="utf-8", newline="") as f:
         csv_writer = csv.writer(f)
 
         csv_writer.writerow(header)
@@ -88,7 +101,7 @@ def export_pokemon(smogon_json: dict, dst_path: Path) -> None:
     ]
 
     rows = []
-    for pkmn_dict in smogon_json["injectRpcs"][1][1]["pokemon"]:
+    for pkmn_dict in smogon_json["pokemon"]:
         row = [
             pkmn_dict["name"],
             pkmn_dict["oob"]["dex_number"],
@@ -101,12 +114,15 @@ def export_pokemon(smogon_json: dict, dst_path: Path) -> None:
             pkmn_dict["spd"],
             pkmn_dict["spe"],
         ]
-
         rows.append(row)
+
+    if len(rows) not in (151, 251):
+        raise ValueError(f"Expected 151 or 251 pokemons, got {len(rows)}")
 
     rows.sort(key=lambda x: x[1])  # Sort by dex number
 
-    with dst_path.open("w", encoding="utf-8") as f:
+    # Specifying newline avoids extra lines on Windows
+    with dst_path.open("w", encoding="utf-8", newline="") as f:
         csv_writer = csv.writer(f)
 
         csv_writer.writerow(header)
@@ -123,13 +139,14 @@ def export_items(smogon_json: dict, dst_path: Path) -> None:
     ]
 
     rows = []
-    for item_dict in smogon_json["injectRpcs"][1][1]["items"]:
+    for item_dict in smogon_json["items"]:
         row = [item_dict[col] for col in header]
         rows.append(row)
 
     rows.sort(key=lambda x: x[0])  # Sort by name
 
-    with dst_path.open("w", encoding="utf-8") as f:
+    # Specifying newline avoids extra lines on Windows
+    with dst_path.open("w", encoding="utf-8", newline="") as f:
         csv_writer = csv.writer(f)
 
         csv_writer.writerow(header)
@@ -143,27 +160,26 @@ def main():
         ("gs", "https://www.smogon.com/dex/gs/pokemon/"),
     ]
 
+    dst_dir = Path(__file__).parents[1] / "data"
+    if not dst_dir.is_dir():
+        print(f"Creating dir at {dst_dir}")
+        dst_dir.mkdir()
+
     for gen_name, gen_url in gen_urls:
-        smogon_json = get_smogon_json_string(gen_url)
-        json_content = json.loads(smogon_json)
+        print(f"Preparing data for gen {gen_name}")
+        smogon_json = get_smogon_json(gen_url)
 
-        script_dir = Path(__file__).parent
-        dst_dir = Path(script_dir.parent, "data")
-        if not dst_dir.is_dir():
-            print(f"Creating dir at {dst_dir}")
-            dst_dir.mkdir()
+        csv_path = dst_dir / f"smogon_{gen_name}_pokemon.csv"
+        print(f"  Writing pokedex to {csv_path}")
+        export_pokemon(smogon_json=smogon_json, dst_path=csv_path)
 
-        csv_path = Path(dst_dir, f"smogon_{gen_name}_moves.csv")
-        print(f"Writing moves to {csv_path}")
-        export_moves(smogon_json=json_content, dst_path=csv_path)
+        csv_path = dst_dir / f"smogon_{gen_name}_moves.csv"
+        print(f"  Writing movedex to {csv_path}")
+        export_moves(smogon_json=smogon_json, dst_path=csv_path)
 
-        csv_path = Path(dst_dir, f"smogon_{gen_name}_pokemon.csv")
-        print(f"Writing pokemon to {csv_path}")
-        export_pokemon(smogon_json=json_content, dst_path=csv_path)
-
-        csv_path = Path(dst_dir, f"smogon_{gen_name}_items.csv")
-        print(f"Writing items to {csv_path}")
-        export_items(smogon_json=json_content, dst_path=csv_path)
+        csv_path = dst_dir / f"smogon_{gen_name}_items.csv"
+        print(f"  Writing itemdex to {csv_path}")
+        export_items(smogon_json=smogon_json, dst_path=csv_path)
 
     return
 
