0af575c10c393710b950b06a96df07899b39f97c
Dudly01 <24471678+Dudly01@users.noreply.github.com>
2025-01-16 16:19:38 +0100

    Revise locate_screen core example #224

--- core/examples/locate_screen.rs
+++ core/examples/locate_screen.rs
@@ -1,24 +1,15 @@
-//! Prototype app, locating the Gen I-II summary screens.
+//! A dev app used for creating the Game Boy screen-locating functionality.
+
 use core::gameboy::{search_screen_gsc, search_screen_rby};
 use image::{io::Reader as ImageReader, Luma};
 use imageproc::{contrast::threshold_mut, rect::Rect};
-use show_image;
+use show_image::{self};
+use std::io;
 
 #[show_image::main]
 fn main() -> Result<(), String> {
-    let windows = [
-        show_image::create_window("Main 1", Default::default()).unwrap(),
-        show_image::create_window("2", Default::default()).unwrap(),
-        show_image::create_window("3", Default::default()).unwrap(),
-        show_image::create_window("4", Default::default()).unwrap(),
-        show_image::create_window("5", Default::default()).unwrap(),
-    ];
-
-    use std::env;
-    let path = env::current_dir().unwrap();
-    println!("The current directory is {}", path.display());
-
-    let paths = [
+    // Relative to crate root
+    let image_paths = [
         "data/Yellow_summary_1.png",
         "data/Yellow_summary_2.png",
         "data/Crystal_summary_1.png",
@@ -26,28 +17,66 @@ fn main() -> Result<(), String> {
         "data/Crystal_summary_3.png",
     ];
 
-    let imgs = [
-        ImageReader::open(paths[0]).unwrap().decode().unwrap(),
-        ImageReader::open(paths[1]).unwrap().decode().unwrap(),
-        ImageReader::open(paths[2]).unwrap().decode().unwrap(),
-        ImageReader::open(paths[3]).unwrap().decode().unwrap(),
-        ImageReader::open(paths[4]).unwrap().decode().unwrap(),
-    ];
-
-    if paths.len() != imgs.len() || paths.len() != windows.len() {
-        panic!("Mismatching number of paths and images.")
+    if image_paths.len() == 0 {
+        panic!("No images to load!");
     }
 
-    for threshold in (0..255).step_by(10) {
-        println!("Threshold: {}", threshold);
+    let images: Vec<_> = image_paths
+        .iter()
+        .map(|&path| ImageReader::open(path).unwrap().decode().unwrap())
+        .collect();
 
-        for i in 0..5 {
-            let mut img = imgs[i].to_luma8();
-            threshold_mut(&mut img, threshold);
+    let windows: Vec<_> = image_paths
+        .iter()
+        .enumerate()
+        .map(|(i, &path)| {
+            let window_title = format!("Image #{i}: '{path}'");
+            show_image::create_window(&window_title, Default::default()).unwrap()
+        })
+        .collect();
+
+    // Show original images
+    println!("Showing original images");
+    for i in 0..images.len() {
+        let window = &windows[i];
+        let image = images[i].clone();
+        let path = &image_paths[i];
+        let title = format!("Image #{i}: '{path}'");
+
+        window.set_image(title, image).unwrap();
+    }
+
+    // Wait for user input
+    loop {
+        println!("Continue? [Y/n]");
+
+        let mut user_answer = String::new();
+
+        io::stdin()
+            .read_line(&mut user_answer)
+            .expect("Failed to read line");
+
+        let user_answer = user_answer.trim();
+
+        match user_answer {
+            "" | "Y" | "y" => break,
+            "N" | "n" => return Ok(()),
+            _ => continue,
+        }
+    }
+
+    let threshold_values: Vec<u8> = (0..255).step_by(10).collect();
+
+    for threshold_value in threshold_values {
+        println!("Applying a binary threshold of {threshold_value}");
+        println!("Found game versions and screen positions:");
+        for i in 0..images.len() {
+            let mut img = images[i].to_luma8();
+            threshold_mut(&mut img, threshold_value);
             *img.get_pixel_mut_checked(0, 0).unwrap() = Luma([0]);
             let contours = imageproc::contours::find_contours::<i32>(&img);
 
-            print!("{i} ");
+            print!("  #{i}: ");
 
             let screen_candidates = search_screen_rby(&contours);
             let rby_screen_pos = screen_candidates.iter().max_by_key(|&p| p.width);
@@ -90,25 +119,26 @@ fn main() -> Result<(), String> {
             window.set_image(i.to_string(), img).unwrap();
         }
 
-        for event in windows[0].event_channel().unwrap() {
-            if let show_image::event::WindowEvent::KeyboardInput(event) = event {
-                if event.input.key_code == Some(show_image::event::VirtualKeyCode::Escape)
-                    && event.input.state.is_pressed()
-                {
-                    return Ok(());
-                }
+        // Wait for user input
+        loop {
+            println!("Continue? [Y/n]");
 
-                if event.input.key_code == Some(show_image::event::VirtualKeyCode::Space)
-                    && event.input.state.is_pressed()
-                {
-                    break;
-                }
+            let mut user_answer = String::new();
+
+            io::stdin()
+                .read_line(&mut user_answer)
+                .expect("Failed to read line");
+
+            let user_answer = user_answer.trim();
+
+            match user_answer {
+                "" | "Y" | "y" => break,
+                "N" | "n" => return Ok(()),
+                _ => continue,
             }
         }
     }
 
-    // Print keyboard events until Escape is pressed, then exit.
-    // If the user closes the window, the channel is closed and the loop also exits.
-
+    println!("Done");
     Ok(())
 }
